2646472f66b79d851532c620744fddf39f1e4f74
Alyza Shane Lumay <lumayshane1704@gmail.com>
2025-04-16 15:00:05 +0800

    GANA NA plss

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from database import Base
 
 class Book(Base):
     __tablename__ = "books"
-id = Column(Integer, primary_key=True, index=True)
-title = Column(String, index=True)
-author = Column(String)
+
+    id = Column(Integer, primary_key=True, index=True)  # ✅ REQUIRED!
+    title = Column(String, index=True)
+    author = Column(String, index=True)
